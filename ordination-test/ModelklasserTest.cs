62ee9e1509edafbcc71aefea652ce72000a08877
J0Money <eaa24joah@students.eaaa.dk>
2025-12-02 14:08:24 +0100

    public void TestDoegnDosisUdenforOrdination() rettet

--- ordination-test/ModelklasserTest.cs
+++ ordination-test/ModelklasserTest.cs
@@ -23,12 +23,14 @@ public class ModelklasserTest
     [TestMethod]
     public void TestDoegnDosisUdenforOrdination()
     {
-        PN pn = new PN(new DateTime(2025, 11, 30),
-            new DateTime(2025, 11, 28),
-            10);
-        
         Assert.ThrowsException<ArgumentException>(() =>
         {
+            PN pn = new PN(
+                new DateTime(2025, 11, 30), 
+                new DateTime(2025, 11, 28), 
+                10
+            );
+            
             pn.doegnDosis();
         });
 
